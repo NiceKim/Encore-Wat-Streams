4242f85778fae470ee9aa98a19d7336ec791cfa8
password_stars <dajin.kim@my.jcu.edu.au>
2025-06-10 19:48:49 +0800

    Update API

---
+++ backend/controllers/bookingController.js
@@ -0,0 +1,32 @@
+const db = require('../../database/sample_db.js');
+
+// Get all bookings
+const getBookings = (req, res) => {
+    // req.user.User_ID
+    // next();
+    console.log(req.user.id)
+    const bookings = db.getBookings(req.user.id);
+    res.json(bookings);
+  };
+  
+  // Create a new booking
+  const createBooking = (req, res) => {
+    const { Date, User_ID, Show_ID } = req.body;
+    if (!Date || !User_ID || !Show_ID) {
+      return res.status(400).json({ message: 'Field are missing.' });
+    }
+    const newBooking = db.createBooking({ Date, User_ID, Show_ID });
+    res.status(201).json(newBooking);
+  };
+  
+  // Delte a booking
+  const deleteBooking = (req, res) => {
+    const { id } = req.params;
+    const result = db.deleteBooking(id);
+    if (!result) {
+      return res.status(404).json({ message: 'Booking not found.' });
+    }
+    res.json({ message: 'Booking deleted.' });
+  };
+  
+  module.exports = { getBookings, createBooking, deleteBooking }; 
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,16 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const userRoutes = require('./routes/userRoutes');
 const showRoutes = require('./routes/showRoutes');
-
+const theaterRoutes = require('./routes/theaterRoutes');
+const bookingRoutes = require('./routes/bookingRoutes');
 const app = express();
 app.use(bodyParser.json());
 
-// Register Routes
+
 app.use('/api/users', userRoutes);
 app.use('/api/shows', showRoutes);
+app.use('/api/theater', theaterRoutes);
+app.use('/api/bookings', bookingRoutes);
 
 const PORT = 3000;
 app.listen(PORT, () => console.log(`Server Executing: http://localhost:${PORT}`));
